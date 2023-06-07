54bd7a17ce7b05c05c7a67445dd3ff76fe07d0ea
senlin <gougewh@sina.com>
2023-06-08 01:03:16 +0800

    开发字幕提交功能总

--- src/pages/fragment-manage/index.tsx
+++ src/pages/fragment-manage/index.tsx
@@ -1,16 +1,22 @@
 import { useQueryClient, useQuery, useMutation } from '@tanstack/react-query'
 import { getFragment } from '@/api/film'
 import { useParams } from 'react-router-dom'
-import { Form, Select, Slider } from 'antd'
+import { Form, Select, Slider, Button, Input } from 'antd'
 import { useState, useEffect } from 'react'
 import videojs from 'video.js'
 import zhLang from 'video.js/dist/lang/zh-CN.json'
 import Player from 'video.js/dist/types/player'
 import { PlayCircleOutlined, PauseCircleOutlined } from '@ant-design/icons'
 import '@/assets/style/video-reset.css'
+import copy from 'copy-to-clipboard'
 
 videojs.addLanguage('zh-CN', zhLang)
-
+interface CaptionProp {
+  start: string
+  end: string
+  en: string
+  cn: string
+}
 const FragmentMange = () => {
   const queryClient = useQueryClient()
   const [ player, setPlayer ] = useState<Player>()
@@ -18,6 +24,7 @@ const FragmentMange = () => {
   const { fragmentId } = useParams()
   const { data, isLoading } = useQuery({queryKey: ['getFragment', fragmentId], queryFn: () => getFragment(fragmentId!)})
   const [form, setForm] = useState({ value: 1 })
+  const [caption, setCaption] = useState<CaptionProp[]>([])
   const [durationTime, setDurationTime] = useState(0)
   const [currentTime, setCurrentTime] = useState(0)
   const [slidrTime, setSlidrTime] = useState(0)
@@ -115,6 +122,20 @@ const FragmentMange = () => {
     const s = Math.floor(time % 60).toString()
     return m.padStart(2, '0') + ":" + s.padStart(2, '0')
   }
+  const setTime = (millisecond: number) => () => {
+    if (!player) return
+    if (!player.paused()) {
+      player.pause() 
+    }
+    const currentTime = player.currentTime()
+    player!.currentTime(currentTime + (millisecond / 1000))
+  }
+  function _copy() {
+    copy((currentTime * 1000).toFixed(0))
+  }
+  function onFinish(values: CaptionProp) {
+    console.log(values);
+  }
   return <div>
     <Form inline>
       <Form.Item label='倍速' w-45>
@@ -125,7 +146,7 @@ const FragmentMange = () => {
         </Select>
       </Form.Item>
     </Form>
-    <div relative w-240>
+    <div relative w-240 m-b-2>
       <video 
         w-240
         h-auto
@@ -144,7 +165,7 @@ const FragmentMange = () => {
         }
       </div>
     </div>
-    <div flex flex-items-center w-240>
+    <div flex flex-items-center w-240 m-b-3>
       <span m-r-2>{getFormattTime(currentTime)}</span>
       <div flex-1 >
         <Slider 
@@ -157,10 +178,52 @@ const FragmentMange = () => {
       </div>
       <span m-l-2>{getFormattTime(durationTime)}</span>
     </div>
-    <div>
-      <span></span>
+    <div m-b-7 flex flex-items-center>
+      <p m-r-1 w-25>{(currentTime * 1000).toFixed(0)}毫秒</p>
+      <Button m-r-15 type='primary' size='small' onClick={_copy}>复制秒数</Button>
+      <Button m-r-3 type='primary' size='small' onClick={setTime(-500)}>减500毫秒</Button>
+      <Button m-r-3 type='primary' size='small' onClick={setTime(-200)}>减200毫秒</Button>
+      <Button m-r-15 type='primary' size='small' onClick={setTime(-50)}>减50毫秒</Button>
+      <Button m-r-3 type='primary' size='small' onClick={setTime(50)}>加50毫秒</Button>
+      <Button m-r-3 type='primary' size='small' onClick={setTime(200)}>加200毫秒</Button>
+      <Button type='primary' size='small' onClick={setTime(500)}>加500毫秒</Button>
     </div>
-    {JSON.stringify(data)}
+    <Form 
+      labelCol={{span: 3}} 
+      wrapperCol={{span: 8}} 
+      labelAlign='left' 
+      requiredMark={false}
+      w-170
+      onFinish={onFinish}
+    >
+      <Form.Item 
+        label='开始时间' 
+        name='start' 
+        rules={[{ required: true, message: '请输入开始时间' }]}
+      >
+        <Input w-50 placeholder='开始时毫秒数' />
+      </Form.Item>
+      <Form.Item 
+        label='结束时间' 
+        name='end'
+        rules={[{ required: true, message: '请输入结束时间' }]}
+      >
+        <Input w-50 placeholder='结束时毫秒数' />
+      </Form.Item>
+     <Form.Item label='英文原文'name='en'>
+        <Input.TextArea placeholder='请输入英文原文' />
+      </Form.Item>
+      <Form.Item label='中文译文' name='cn'>
+        <Input.TextArea placeholder='请输入中文译文' />
+      </Form.Item>
+      <Form.Item labelCol={{span: 3}} wrapperCol={{span: 8, offset: 3}}>
+        <div>
+          <Button m-r-2>取消</Button>
+          <Button type='primary' htmlType='submit'>确认</Button>
+        </div>
+      </Form.Item>
+    </Form>
+    {/* {JSON.stringify(data)} */}
     {/* <input onKeyDown={(event: any) => event.stopPropagation()} /> */}
   </div>
 }
