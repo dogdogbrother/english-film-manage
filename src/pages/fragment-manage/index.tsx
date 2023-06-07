a21fabcb1037545a178d8787ff82b9ed83d0e906
王浩 <wanghao@wanghaodeMac-mini.local>
2023-06-07 19:29:21 +0800

    增加视频进度条

--- src/pages/fragment-manage/index.tsx
+++ src/pages/fragment-manage/index.tsx
@@ -1,7 +1,7 @@
 import { useQueryClient, useQuery, useMutation } from '@tanstack/react-query'
 import { getFragment } from '@/api/film'
 import { useParams } from 'react-router-dom'
-import { Form, Select } from 'antd'
+import { Form, Select, Slider } from 'antd'
 import { useState, useEffect } from 'react'
 import videojs from 'video.js'
 import zhLang from 'video.js/dist/lang/zh-CN.json'
@@ -18,6 +18,9 @@ const FragmentMange = () => {
   const { fragmentId } = useParams()
   const { data, isLoading } = useQuery({queryKey: ['getFragment', fragmentId], queryFn: () => getFragment(fragmentId!)})
   const [form, setForm] = useState({ value: 1 })
+  const [durationTime, setDurationTime] = useState(0)
+  const [currentTime, setCurrentTime] = useState(0)
+  const [slidrTime, setSlidrTime] = useState(0)
   useEffect(() => {
     if (data) {
       const _player: Player = videojs('video-play', {
@@ -38,7 +41,29 @@ const FragmentMange = () => {
   useEffect(() => {
     if (!player) return
     registerVideoEvent(player)
+    window.addEventListener('keydown', keydown)
+    return () => {
+      window.removeEventListener('keydown', keydown);
+    }
   }, [player])
+  function keydown(e: any) {
+    if (!player) return
+    const currentTime = player.currentTime()
+    // 32是空格 37是左 39是右
+    if (e.keyCode === 32) {
+      if (player.paused()) {
+        player.play() 
+      } else {
+        player.pause()
+      }
+    }
+    if (e.keyCode === 37) {
+      player.currentTime(currentTime - 2)
+    }
+    if (e.keyCode === 39) {
+      player.currentTime(currentTime + 2)
+    }
+  }
   function registerVideoEvent(_player: Player) {
     _player.on('play', () => {
       setPlayState(true)
@@ -47,23 +72,23 @@ const FragmentMange = () => {
       setPlayState(false)
     })
     _player.on('timeupdate', (e: Event) => {
-      // const target =  e.target as any
-      // const { currentTime, duration: _duration  } = target.children[0] || {}
-      // setCurrentTime(currentTime)
-      // if (duration === 0) {
-      //   setDuration(_duration)
-      // }
+      const target =  e.target as any
+      const { currentTime, duration  } = target.children[0] || {}
+      setCurrentTime(currentTime)
+      if (durationTime === 0) {
+        setDurationTime(duration)
+      }
     })
   }
   const playIconProp = {
-    color: '#fff',
     style: {
       position: 'absolute', 
       top: '50%', 
       left: '50%', 
       transform: 'translate(-50%, -50%)',
       fontSize: '50px',
-      opacity: 0.8
+      opacity: 0.8,
+      color: '#fff'
     }
   }
   function onPlay() {
@@ -75,6 +100,20 @@ const FragmentMange = () => {
   function handlePlaybackRates(value: number) {
     setForm({...form, value})
     player!.playbackRate(value)
+  }
+  function changeSlider(value: number) {
+    setSlidrTime(value)
+  }
+  function onAfterChange(value: number) {
+    player!.currentTime(value / 10000)
+    setCurrentTime(value / 10000)
+    setSlidrTime(0)
+  }
+  // 传入毫秒 设置 00:00 格式
+  function getFormattTime(time: number) {
+    const m = Math.floor(time / 60).toString()
+    const s = Math.floor(time % 60).toString()
+    return m.padStart(2, '0') + ":" + s.padStart(2, '0')
   }
   return <div>
     <Form inline>
@@ -105,7 +144,24 @@ const FragmentMange = () => {
         }
       </div>
     </div>
+    <div flex flex-items-center w-240>
+      <span m-r-2>{getFormattTime(currentTime)}</span>
+      <div flex-1 >
+        <Slider 
+          max={durationTime * 10000} 
+          tooltip={{formatter: () => getFormattTime(currentTime)}} 
+          value={slidrTime || currentTime * 10000}
+          onChange={changeSlider}
+          onAfterChange={onAfterChange}
+        />
+      </div>
+      <span m-l-2>{getFormattTime(durationTime)}</span>
+    </div>
+    <div>
+      <span></span>
+    </div>
     {JSON.stringify(data)}
+    {/* <input onKeyDown={(event: any) => event.stopPropagation()} /> */}
   </div>
 }
 export default FragmentMange
